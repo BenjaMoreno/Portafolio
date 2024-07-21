b3499dab6b99c080d12a89bf371f85a654d910ee
BenjaMoreno <webenja0@gmail.com>
2024-07-21 01:16:36 -0400

    Header con React Router listo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import LOGO from "../src/IMAGES/logoben.png";
+import { QuienSoy } from "./PAGES/Quien Soy/QuienSoy";
+import { Codificando } from "./PAGES/Codificando/Codificando";
+import { Proyectos } from "./PAGES/Proyectos/Proyectos";
+import { Contactame } from "./PAGES/Contactame/Contactame";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <nav>
+            <img src={LOGO} id="LOGO" />
+            <ul>
+              <li>
+                <NavLink to="/" className="navlink">
+                  ¿Quién soy?
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/codificando" className="navlink">
+                  Codificando
+                </NavLink>
+              </li>
+              <li></li>
+              <li>
+                <NavLink to="/proyectos" className="navlink">
+                  Proyectos
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/contactame" className="navlink">
+                  Contáctame
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div className="container-hrs-header">
+            <hr className="hr-header"></hr>
+            <hr className="hr-header"></hr>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<QuienSoy />} />
+            <Route path="/codificando" element={<Codificando />} />
+            <Route path="/proyectos" element={<Proyectos />} />
+            <Route path="/contactame" element={<Contactame />} />
+          </Routes>
+        </main>
+        <footer></footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/PAGES/Codificando/Codificando.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Codificando.css'
+
+export const Codificando = () => {
+  return (
+    <div>Codificando</div>
+  )
+}
