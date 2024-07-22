7747a9ccb5c3107cd705c7d8937470e077bc1f14
BenjaMoreno <webenja0@gmail.com>
2024-07-21 23:00:05 -0400

    Footer listo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import LOGO from "../src/IMAGES/logoben.png";
 import { QuienSoy } from "./PAGES/Quien Soy/QuienSoy";
-import { Codificando } from "./PAGES/Codificando/Codificando";
 import { Proyectos } from "./PAGES/Proyectos/Proyectos";
 import { Contactame } from "./PAGES/Contactame/Contactame";
-
+import { FrontendPro } from "./PAGES/Frontend pro/FrontendPro";
+import facebook from "../src/IMAGES/SVG/FACEBOOK.svg";
+import github from "../src/IMAGES/SVG/GITHUB.svg";
+import linkedin from "../src/IMAGES/SVG/LINKEDIN.svg";
 function App() {
   return (
     <BrowserRouter>
@@ -45,12 +47,32 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<QuienSoy />} />
-            <Route path="/codificando" element={<Codificando />} />
+            <Route path="/codificando" element={<FrontendPro />} />
             <Route path="/proyectos" element={<Proyectos />} />
             <Route path="/contactame" element={<Contactame />} />
           </Routes>
         </main>
-        <footer></footer>
+        <footer>
+          <div className="container-hrs-footer">
+            <hr className="hr-footer" />
+            <hr className="hr-footer" />
+          </div>
+          <div className="container-frase-footer">
+            Viviendo, aprendiendo y subiendo de nivel un día a la vez.
+          </div>
+          <div className="container-rrss">
+            <a href="https://web.facebook.com/benjamin.morenosaez.3" target="blank">
+              <img src={facebook} alt="logo face" />
+            </a>
+            <a href="https://www.linkedin.com/in/benjam%C3%ADn-moreno-s%C3%A1ez-frontenddeveloper/" target="blank">
+              <img src={linkedin} alt="logo linkedin" />
+            </a>
+            <a href="https://github.com/BenjaMoreno" target="blank"> 
+              <img src={github} alt="logo github" />
+            </a>
+          </div>
+          <img className="logo-footer" src={LOGO} />
+        </footer>
       </div>
     </BrowserRouter>
   );
