f525e51f7f642e0c90f7fb1b6672e95ed054ec6c
BenjaMoreno <webenja0@gmail.com>
2024-07-31 13:22:10 -0400

    validacion con formik y yup lista

--- src/App.js
+++ src/App.js
@@ -9,89 +9,94 @@ import facebook from "../src/IMAGES/SVG/FACEBOOK.svg";
 import github from "../src/IMAGES/SVG/GITHUB.svg";
 import linkedin from "../src/IMAGES/SVG/LINKEDIN.svg";
 import { ReactComponent as FLECHA } from "../src/IMAGES/SVG/FLECHA.svg";
+import { ContextUser } from "./CONTEXT/ContextUser";
 function App() {
   //ACTIVAR BOTON DE SUBIDA HREF=#
   window.addEventListener("scroll", function () {
     var btn_flecha = document.getElementById("subir");
     btn_flecha.style.display = window.scrollY > 0 ? "block" : "none";
   });
+
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <header>
-          <nav>
-            <img src={LOGO} id="LOGO" />
-            <ul>
-              <li>
-                <NavLink to="/" className="navlink">
-                  ¿Quién soy?
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/codificando" className="navlink">
-                  Frontend Pro
-                </NavLink>
-              </li>
-              <li></li>
-              <li>
-                <NavLink to="/proyectos" className="navlink">
-                  Proyectos
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/contactame" className="navlink">
-                  Contáctame
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-          <div className="container-hrs-header">
-            <hr className="hr-header"></hr>
-            <hr className="hr-header"></hr>
-          </div>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<QuienSoy />} />
-            <Route path="/codificando" element={<FrontendPro />} />
-            <Route path="/proyectos" element={<Proyectos />} />
-            <Route path="/contactame" element={<Contactame />} />
-          </Routes>
-        </main>
-        <footer>
-          <div className="container-hrs-footer">
-            <hr className="hr-footer" />
-            <hr className="hr-footer" />
-          </div>
-          <div className="container-frase-footer">
-            Viviendo, aprendiendo y subiendo de nivel un día a la vez.
-          </div>
-          <div className="container-rrss">
-            <a
-              href="https://web.facebook.com/benjamin.morenosaez.3"
-              target="blank"
-            >
-              <img src={facebook} alt="logo face" />
+    <ContextUser.Provider usuario={''}>
+      <BrowserRouter>
+        <div className="App">
+          <header>
+            <nav>
+              <img src={LOGO} id="LOGO" />
+              <ul>
+                <li>
+                  <NavLink to="/" className="navlink">
+                    ¿Quién soy?
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/codificando" className="navlink">
+                    Frontend Pro
+                  </NavLink>
+                </li>
+                <li></li>
+                <li>
+                  <NavLink to="/proyectos" className="navlink">
+                    Proyectos
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contactame" className="navlink">
+                    Contáctame
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+            <div className="container-hrs-header">
+              <hr className="hr-header"></hr>
+              <hr className="hr-header"></hr>
+            </div>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<QuienSoy />} />
+              <Route path="/codificando" element={<FrontendPro />} />
+              <Route path="/proyectos" element={<Proyectos />} />
+              <Route path="/contactame" element={<Contactame />} />
+            </Routes>
+          </main>
+          <footer>
+            <div className="container-hrs-footer">
+              <hr className="hr-footer" />
+              <hr className="hr-footer" />
+            </div>
+            <div className="container-frase-footer">
+              Viviendo, aprendiendo y subiendo de nivel un día a la vez.
+            </div>
+            <div className="container-rrss">
+              <a
+                href="https://web.facebook.com/benjamin.morenosaez.3"
+                target="blank"
+              >
+                <img src={facebook} alt="logo face" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/benjam%C3%ADn-moreno-s%C3%A1ez-frontenddeveloper/"
+                target="blank"
+              >
+                <img src={linkedin} alt="logo linkedin" />
+              </a>
+              <a href="https://github.com/BenjaMoreno" target="blank">
+                <img src={github} alt="logo github" />
+              </a>
+            </div>
+            <a href="#">
+              <img className="logo-footer" src={LOGO} />
             </a>
-            <a
-              href="https://www.linkedin.com/in/benjam%C3%ADn-moreno-s%C3%A1ez-frontenddeveloper/"
-              target="blank"
-            >
-              <img src={linkedin} alt="logo linkedin" />
-            </a>
-            <a href="https://github.com/BenjaMoreno" target="blank">
-              <img src={github} alt="logo github" />
-            </a>
-          </div>
-          <a href="#">
-            <img className="logo-footer" src={LOGO} />
+          </footer>
+          <a href="#" id="subir">
+            <FLECHA className="flecha-svg" />
           </a>
-        </footer>
-        <a href="#" id="subir">
-          <FLECHA className="flecha-svg" />
-        </a>
-      </div>
-    </BrowserRouter>
+        </div>
+      </BrowserRouter>
+    </ContextUser.Provider>
   );
 }
 
--- src/COMPONENTES/frontend pro/GESTION DE ESTADO/Gestion.js
+++ src/COMPONENTES/frontend pro/GESTION DE ESTADO/Gestion.js
@@ -1,18 +1,43 @@
 import React from "react";
 import "./Gestion.css";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import astronaut from "../../../IMAGES/SVG/ASTRONAUT.svg";
 import detective from "../../../IMAGES/SVG/DETECTIVE.svg";
 import barwoman from "../../../IMAGES/SVG/BARWOMAN.svg";
 import cientifica from "../../../IMAGES/SVG/CIENTIFICA.svg";
 export const Gestion = () => {
+  //validacion formulario
+  const formik = useFormik({
+    initialValues: {
+      usuario: "",
+    },
+    validationSchema: Yup.object().shape({
+      usuario: Yup.string().max(10, "Demasiado largo"),
+    }),
+    onSubmit: (values) => {
+      console.log(values.usuario);
+    },
+  });
+
   return (
     <section className="container-gestion">
       <h1 className="titulo-gestion">Gestión de Estado</h1>
-      <form className="form-estado">
+      <form className="form-estado" onSubmit={formik.handleSubmit}>
         <h1>Crea un usuario</h1>
         <div className="container-input-btn">
-          <input placeholder="estado..." />
-          <button>Crear</button>
+          <input
+            type="text"
+            placeholder="estado..."
+            name="usuario"
+            value={formik.values.usuario}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.errors.usuario && formik.touched.usuario && (
+            <span>{formik.errors.usuario}</span>
+          )}
+          <button type="submit">Crear</button>
         </div>
         <h2>Elige tu avatar</h2>
         <div className="container-avatars">
@@ -28,7 +53,8 @@ export const Gestion = () => {
           Usuario:&nbsp;&nbsp;<span>Benjamin</span>
         </div>
         <div className="caja-flex-estado">
-          Avatar:&nbsp;&nbsp;&nbsp;&nbsp;<img src={astronaut} alt="" />
+          Avatar:&nbsp;&nbsp;&nbsp;&nbsp;
+          <img src={astronaut} alt="" />
         </div>
       </div>
     </section>
