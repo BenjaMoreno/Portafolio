3388a8bfc15af77c888432519ab37413df115943
BenjaMoreno <webenja0@gmail.com>
2024-09-22 22:10:52 -0300

    Sección hero en página "¿Quien soy?" con modelo base listo,falta animar y agregar cv de descarga

--- src/PAGES/Quien Soy/QuienSoy.js
+++ src/PAGES/Quien Soy/QuienSoy.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./QuienSoy.css";
+import foto_hero from "../../IMAGES/generales/foto-hero.jpg";
 
 export const QuienSoy = () => {
   return (
-    <div>QuienadSoy</div>
-  )
-}
+    <div className="Hero">
+      <img className="foto-hero" />
+      <div>
+        <h1>Benjamin Moreno</h1>
+        <h2>Desarrollador Frontend</h2>
+      </div>
+    </div>
+  );
+};
