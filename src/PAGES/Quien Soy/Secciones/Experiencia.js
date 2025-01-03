e17c0efe38ce885e0e559c4c83b70ee7401da0b6
BenjaMoreno <webenja0@gmail.com>
2025-01-03 18:59:48 -0300

    Añadimos nueva experiencia y completamos sus casillas

--- src/PAGES/Quien Soy/Secciones/Experiencia.js
+++ src/PAGES/Quien Soy/Secciones/Experiencia.js
@@ -8,27 +8,50 @@ export const Experiencia = () => {
 
   const trabajos = [
     {
-      tipo: "Freelance",
-      Descripcion: "Desarrollo de una revista local para el sur de Chile",
-      fecha: "ene.2024 - dic.2024",
+      tipo: "Práctica laboral",
+      Descripcion: "NoCountry - Desarrollo de un sitio web e-commerce",
+      fecha: "nov.2024 - presente",
       caracteristicas: {
-        primera: "Creación del sitio",
-        segunda: "Optimización y rendimiento del sitio",
-        tercera: "Página SPA (Single Page Aplication)",
-        cuarta: "cuarta",
-        quinta: "quinta",
+        primera: `Desarrollé completamente el sitio web de tipo "e-commerce" sobre productos artesanales,para el cuidado personal`,
+        segunda:
+          "Integré y consumí APIs, apoyé y resolví problemas constantemente en Backend",
+        tercera:
+          "Logré una mejora del 50% en el rendimiento del sitio mediante la optimización de imágenes",
+        cuarta:
+          "Traduje diseños UX/UI a código funcional.Maqueté e implementé estilos en CSS y desarrollé la lógica de los componentes.",
+        quinta:
+          "Participé en dailies cinco veces a la semana, así como en reuniones técnicas con roles clave del equipo (Backend, Frontend, UX/UI, QA, Project Manager y Team Leader).",
+        sexta:
+          "Colaboré de manera efectiva con el equipo, logrando conectarnos, generar un buen ambiente, y poner en marcha todo lo planicado. Habilidades como comunicación efectiva, escucha activa, proactividad y compromiso fueron fundamentales para el éxito del proyecto.",
+        septima: "Responsive Design",
       },
     },
     {
-      tipo: "Simulación laboral",
+      tipo: "Práctica laboral",
       Descripcion: "NoCountry - Desarrollo de una Red Social",
-      fecha: "sept.30 - oct.",
+      fecha: "sept.2024 - nov.2024",
       caracteristicas: {
-        primera: "primera",
-        segunda: "segunda",
-        tercera: "tercera",
-        cuarta: "cuarta",
-        quinta: "quinta",
+        primera: "Diseñé y desarrollé el sitio",
+        segunda: "Optimizé y mejoré rendimiento del sitio",
+        tercera: "Implementé navegacióna avanzada (SPA)",
+        cuarta: "Desarrollo de lógica y reutilización de componentes",
+        quinta:
+          "Implementé funcionalidades (Login,Navegación SPA,sistema de publicaciones,etc..).",
+        sexta: "Colaboré estrechamente con Backends, Frontend y Teamleader.",
+      },
+    },
+    {
+      tipo: "Freelance",
+      Descripcion: "Desarrollo de una revista local para el sur de Chile",
+      fecha: "ene.2022 - dic.2023",
+      caracteristicas: {
+        primera: "Diseño y desarrollo del sitio",
+        segunda: "Optimización y rendimiento del sitio",
+        tercera: "Página SPA (Single Page Aplication)",
+        cuarta: "Validación de formularios",
+        quinta: "Componentes reutilizables",
+        sexta: "Testeo Unitario y E2E del sitio",
+        septima: "Responsive Design",
       },
     },
   ];
@@ -56,9 +79,11 @@ export const Experiencia = () => {
               <div className="detalles-trabajo">
                 <h2>Características del sitio:</h2>
                 <ul>
-                  <li>{trabajo.caracteristicas.primera}</li>
-                  <li>{trabajo.caracteristicas.segunda}</li>
-                  <li>{trabajo.caracteristicas.tercera}</li>
+                  {Object.entries(trabajo.caracteristicas).map(
+                    ([clave, valor], index) => (
+                      <li key={index}>{valor}</li>
+                    )
+                  )}
                 </ul>
               </div>
             ) : (
