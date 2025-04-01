359846a777e31a3d4491b7af9f4717c9a3608b6d
BenjaMoreno <webenja0@gmail.com>
2025-03-31 22:39:29 -0300

    responsive hero

--- src/PAGES/Quien Soy/QuienSoy.js
+++ src/PAGES/Quien Soy/QuienSoy.js
@@ -4,9 +4,9 @@ import { Stack } from "./Secciones/Stack";
 import { SobreMi } from "./Secciones/SobreMi";
 import { Experiencia } from "./Secciones/Experiencia";
 import { Certificados } from "./Secciones/Certificados";
-import foto from "../../IMAGES/generales/WhatsApp Image 2025-03-28 at 11.24.15 PM (1).jpeg";
+import foto from "../../IMAGES/generales/WhatsApp Image 2025-03-31 at 9.07.02 PM.jpeg";
 import { ReactComponent as Github } from "../../IMAGES/SVG/GITHUB.svg";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export const QuienSoy = () => {
   // Efecto typewriter
@@ -56,25 +56,76 @@ export const QuienSoy = () => {
   return (
     <div className="container-quiensoy">
       <div className="Hero">
-        <img src={foto} alt="" className="foto-hero" />
+        <Box
+          sx={{
+            // border: "solid #57d7d7 1px",
+            // border: "solid #2563EB 9px",
+
+            // height: "max-content",
+            // width: "25vw",
+            // maxHeight: "25vh",
+            // height: "auto",
+            // width: "auto",
+            // maxWidth: "70vw",
+            borderRadius: "20px",
+            overflow: "hidden",
+            boxShadow: "0 0 30px lightblue",
+            width: {
+              xs: "auto",
+              sm: "30vw",
+              md: "30vw",
+              lg: "25vw",
+              xl: "25vw",
+            },
+            height: {
+              xs: "40vh",
+              sm: "auto",
+              md: "auto",
+              lg: "auto",
+              xl: "auto",
+            },
+          }}
+        >
+          <img src={foto} alt="" className="foto-hero" />
+        </Box>
         <div className="container-texto-hero">
-          <h2>
+          <Typography
+            variant="h2"
+            sx={{
+              fontWeight: {
+                xs: 700,
+                sm: 300,
+                md: 300,
+                lg: 300,
+                xl: 300,
+              },
+              fontSize: {
+                xs: "1rem !important",
+                sm: "1rem !important",
+                md: "1.8rem !important",
+                lg: "1.8rem !important",
+                xl: "2rem !important",
+              },
+              fontFamily: "var(--fuente-poopins)",
+              alignSelf: "",
+            }}
+          >
             {texto}
             <span style={{ animation: "blink 1s infinite" }}>|</span>
-          </h2>
+          </Typography>
           <Typography
             variant="h1"
             sx={{
               fontWeight: {
-                xs: "700",
+                xs: "800",
                 sm: "700",
                 md: "700",
                 lg: "700",
                 xl: "700",
               },
               fontSize: {
-                xs: "2rem !important",
-                sm: "2rem !important",
+                xs: "2.4rem !important",
+                sm: "3rem !important",
                 md: "4rem !important",
                 lg: "6rem !important",
                 xl: "8rem !important",
